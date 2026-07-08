725fd2c8d26aed294dede8aafa4d784a88caa1dc
ekrish <krishdevopsaws12@gmail.com>
2026-07-08 10:52:59 +0530

    Run terraform fmt

--- eks/backend.tf
+++ eks/backend.tf
@@ -7,10 +7,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket         = "krishna-terraform-state-2026"
-    region         = "us-east-1"
-    key            = "eks/terraform.tfstate"
-    encrypt        = true
+    bucket  = "krishna-terraform-state-2026"
+    region  = "us-east-1"
+    key     = "eks/terraform.tfstate"
+    encrypt = true
   }
 }
 
